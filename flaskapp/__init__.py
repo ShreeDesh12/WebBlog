630663319691085d4f3aa2cde3883bc8fba95e68
Shreeniwas Deshpande <shree.desh72@gmail.com>
2020-10-17 19:22:55 +0530

    Update __init__.py

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__, static_folder = "static")
@@ -7,5 +9,10 @@ app.config['SECRET_KEY'] = '7dchd82hcjjs92nd'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 #app.config["SQLALchemy_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category= 'info'
+bcrypt = Bcrypt(app)
+
 
 from flaskapp import routes
